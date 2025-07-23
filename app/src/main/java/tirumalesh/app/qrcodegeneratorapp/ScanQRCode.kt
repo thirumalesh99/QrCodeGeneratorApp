7e645ea7d55f4ede0326cac05fe18651ff695d16
thirumalesh99 <thirumaleshlucky1999@gmail.com>
2025-07-24 03:04:10 +0530

    Implemented scan qr code and generate qr code functionality

--- app/src/main/java/tirumalesh/app/qrcodegeneratorapp/ScanQRCode.kt
+++ app/src/main/java/tirumalesh/app/qrcodegeneratorapp/ScanQRCode.kt
@@ -1,5 +1,30 @@
 package tirumalesh.app.qrcodegeneratorapp
 
+import android.graphics.Bitmap
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+
 class ScanQRCode {
 
+    fun generateQRCode(content: String): Bitmap? {
+        return try {
+            val writer = MultiFormatWriter()
+            val bitMatrix = writer.encode(content, BarcodeFormat.QR_CODE, 512, 512)
+            val width = bitMatrix.width
+            val height = bitMatrix.height
+            val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
+            for (x in 0 until width) {
+                for (y in 0 until height) {
+                    bitmap.setPixel(
+                        x,
+                        y,
+                        if (bitMatrix[x, y]) android.graphics.Color.BLACK else android.graphics.Color.WHITE
+                    )
+                }
+            }
+            bitmap
+        } catch (e: Exception) {
+            null
+        }
+    }
 }
